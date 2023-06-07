753b24c2e83d3b06b359a9d7aef91da6f9dab84f
mannan <mannanbshah@gmail.com>
2023-06-07 18:52:46 +0530

    added robots.txt plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from '@astrojs/tailwind';
+
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +12,10 @@ export default defineConfig({
     mdx(),
     sitemap(),
     tailwind({
-      config: { applyBaseStyles: false },
+      config: {
+        applyBaseStyles: false,
+      },
     }),
+    robotsTxt({ sitemap: true }),
   ],
 });
